36d3515198f29733a8226da396a553c33e5f6f68
Firat Tale <firat.tale@auto1.com>
2022-06-17 16:34:38 +0300

    add verify func

--- deploy/01-deploy-fund-me.js
+++ deploy/01-deploy-fund-me.js
@@ -3,6 +3,7 @@ const {
   developmentChains,
 } = require("../helper-hardhat-config");
 const { network } = require("hardhat");
+const { verify } = require("../utils/verify");
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
   const { deploy, log, get } = deployments;
@@ -16,12 +17,18 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
   } else {
     ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"];
   }
-
+  const args = [ethUsdPriceFeedAddress];
   const fundMe = await deploy("FundMe", {
     from: deployer,
-    args: [ethUsdPriceFeedAddress], // put price feed address,
+    args, // put price feed address,
     log: true,
   });
+  if (
+    !developmentChains.includes(network.name) &&
+    process.env.ETHERSCAN_API_KEY
+  ) {
+    await verify(fundMe.address, args);
+  }
   log("-----------------------------------------------");
 };
 
