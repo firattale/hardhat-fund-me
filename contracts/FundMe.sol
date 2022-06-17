3ebed80be55708c1b33039d154b2e979db07caa2
Firat Tale <firat.tale@auto1.com>
2022-06-17 17:19:49 +0300

    add natspec

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -4,8 +4,13 @@ pragma solidity ^0.8.4;
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import "./PriceConverter.sol";
 
-error NotOwner();
+error FUNDME_NotOwner();
 
+/** @title A contract for crowd funding
+ * @author Firat Story
+ * @notice This contract is to demo a sample funding contract.
+ * @dev This implements price feeds as our library.
+ */
 contract FundMe {
     using PriceConverter for uint256;
 
@@ -15,11 +20,28 @@ contract FundMe {
     uint256 public constant MINIMUM_USD = 50 * 10**18;
     AggregatorV3Interface public priceFeed;
 
+    modifier onlyOwner() {
+        if (msg.sender != i_owner) revert FUNDME_NotOwner();
+        _;
+    }
+
     constructor(address _priceFeed) {
         i_owner = msg.sender;
         priceFeed = AggregatorV3Interface(_priceFeed);
     }
 
+    receive() external payable {
+        fund();
+    }
+
+    fallback() external payable {
+        fund();
+    }
+
+    /**
+     * @notice This function funds this contract.
+     * @dev This implements price feeds as our library.
+     */
     function fund() public payable {
         require(
             msg.value.getConversionRate(priceFeed) >= MINIMUM_USD,
@@ -27,11 +49,6 @@ contract FundMe {
         );
         addressToAmountFunded[msg.sender] += msg.value;
         funders.push(msg.sender);
-    }
-
-    modifier onlyOwner() {
-        if (msg.sender != i_owner) revert NotOwner();
-        _;
     }
 
     function withdraw() public payable onlyOwner {
@@ -49,13 +66,5 @@ contract FundMe {
             value: address(this).balance
         }("");
         require(callSuccess, "Call failed");
-    }
-
-    fallback() external payable {
-        fund();
-    }
-
-    receive() external payable {
-        fund();
     }
 }
