cdad1bc75ba99fcd0921984d1eef86bafb106eee
Firat Tale <firat.tale@auto1.com>
2022-06-17 13:41:37 +0300

    create deploy mock script

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -5,6 +5,12 @@ const networkConfig = {
   },
 };
 
+const developmentChains = ["hardhat", "localhost"];
+const DECIMALS = 8;
+const INITIAL_ANSWER = 200000000000;
 module.exports = {
   networkConfig,
+  developmentChains,
+  DECIMALS,
+  INITIAL_ANSWER,
 };
