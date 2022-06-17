5a84baee341cf47bac072c0b57d1782597a88f5c
Firat Tale <firat.tale@auto1.com>
2022-06-17 13:07:42 +0300

    modularize priceFeedAddress by chainId

--- deploy/01-deploy-fund-me.js
+++ deploy/01-deploy-fund-me.js
@@ -1,7 +1,14 @@
+const { networkConfig } = require("./helper-hardhat-config");
+
 module.exports = async ({ getNamedAccounts, deployments }) => {
   const { deploy, log } = deployments;
   const { deployer } = await getNamedAccounts();
   const { chainId } = network.config;
-
+  const { ethUsdPriceFeed } = networkConfig[chainId];
   // when going for localhost or hardhat network we want to use a mock
+  const fundMe = await deploy("FundMe", {
+    from: deployer,
+    args: [ethUsdPriceFeedn], // put price feed address,
+    log: true,
+  });
 };
